5312f8d82716a711f636ec1d2a4ee9fa2921d7bc
Slimeball <alexramo2330@gmail.com>
2022-08-03 13:04:24 -0500

    current-weather-display

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,30 +1,30 @@
 var key = '65a6ef9e53b138d495fa2864b093996c';
 var historyArray = [];
 
-function formSubmit(event) {
+var formSubmit = function(event) {
     event.preventDefault();
-    var city = citySearch.value().trim();
+    let city = document.getElementById('citySearch').value.trim();
     if(city) {
         cityData(city);
-        city.value = ' ';
+        city.value = '';
     } else {
         alert('Enter a city');
     }
 };
 
 var cityData = function(city) {
-    var apiURL = `https://api.openweathermap.org/data/3.0/weather?q=${city}&appid=${key}`;
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
     fetch(apiURL).then((response) => {
         if(response.ok) {
             response.json().then((data) => {
                 var lat = data.coord.lat;
                 var lon = data.coord.lon;
-                var callAPI = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${key}`
+                var callAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`
                 fetch(callAPI).then((response) => {
                     response.json().then((data) => {
                         currentWeather(city, data);
-                        futureWeather(data);
-                        console.log(response);
+                        //futureWeather(data);
+                        console.log(data);
                     })
                 })
             })
@@ -42,4 +42,12 @@ function currentWeather(city, data) {
     var currentUvi = data.current.uvi;
 
     var currentWeatherNews = document.querySelector('#city-weather');
+
+    currentWeatherNews.innerHTML = 
+    `<h5>${newCity} (${currentTime})<h4>
+    <p>Temperature: ${currentTemperature.toFixed(2)}&deg;F</p>
+    <p>Humidity: ${currentHumidity}%<p>
+    <p>UV Index: <span id='currentUvi'>${currentUvi}</span></p>`
 }
+
+document.querySelector("#search-btn").addEventListener('click', formSubmit);
